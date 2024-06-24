611ea827c9ee651badd30c6cdeecf2fff3b5ca2e
unknown <huykimcuong5@gmail.com>
2024-06-24 22:47:45 +0700

    Update transaction lock for avoiding deadlock

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -19,7 +19,6 @@ func TestTransferTx(t *testing.T) {
 	// * Run n concurrent transfer transactions
 	n := 5
 	amount := int64(10)
-
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
 
@@ -78,14 +77,10 @@ func TestTransferTx(t *testing.T) {
 		// * Check accounts
 		fromAccount := result.FromAccount
 
-		fmt.Println(result.FromAccount)
-
 		require.NotEmpty(t, fromAccount)
 		require.Equal(t, account1.ID, fromAccount.ID)
 
 		toAccount := result.ToAccount
-
-		fmt.Println(result.ToAccount)
 
 		require.NotEmpty(t, toAccount)
 		require.Equal(t, account2.ID, toAccount.ID)
@@ -121,4 +116,56 @@ func TestTransferTx(t *testing.T) {
 
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+}
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDb)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+
+	n := 10
+	amount := int64(10)
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// * Check the final updated balance
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
 }
