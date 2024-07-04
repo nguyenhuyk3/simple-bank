bd70f848c56e052496df148585d0355256e1929f
unknown <huykimcuong5@gmail.com>
2024-07-04 12:14:55 +0700

    Add basic api for account

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -20,7 +20,7 @@ var testDb *sql.DB
 func TestMain(m *testing.M) {
 	var err error
 
-	testDb, err = sql.Open(dbDriver, dbSource)
+	conn, err = sql.Open(dbDriver, dbSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
